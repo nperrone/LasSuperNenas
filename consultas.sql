68a08a5a8734c12474b719abd4fa7583f6e31e8d
Nicole Perrone <nperrone@udesa.edu.ar>
2024-09-20 20:02:26 -0300

    cambios 5 y 9

--- consultas.sql
+++ consultas.sql
@@ -24,16 +24,18 @@ JOIN productos p ON c.codigo_categoria = p.codigo_categoria
 GROUP BY c.nombre
 ORDER BY total_productos DESC;
 
--- (5) ¿Cuántos usuarios han realizado compras en más de una categoría?
+-- (5) Nombre del top 10 de usuarios que han realizado compras en más de una categoría
 SELECT u.numero_documento, u.nombre, u.apellido, COUNT(DISTINCT p.codigo_categoria) AS total_categorias
 FROM usuarios u
 JOIN pedidos pe ON u.numero_documento = pe.numero_documento
 JOIN pedidos_productos pp ON pe.codigo_pedido = pp.codigo_pedido
 JOIN productos p ON pp.codigo_producto = p.codigo_producto
-GROUP BY u.numero_documento
-HAVING COUNT(DISTINCT p.codigo_categoria) > 1;
+GROUP BY u.numero_documento, u.nombre, u.apellido
+HAVING COUNT(DISTINCT p.codigo_categoria) > 1
+ORDER BY total_categorias DESC
+LIMIT 10;
 
--- (6) ¿Qué usuarios han realizado más de 5 compras durante el último año?
+-- (6) ¿Qué usuarios han realizado más de 5 compras durante el último año? Dar nombre, Apellido y total de compras.
 SELECT u.nombre, u.apellido, COUNT(pe.codigo_pedido) AS total_compras
 FROM usuarios u
 JOIN pedidos pe ON u.numero_documento = pe.numero_documento
@@ -56,13 +58,19 @@ JOIN pedidos_productos ON pedidos.codigo_pedido = pedidos_productos.codigo_pedid
 JOIN productos ON pedidos_productos.codigo_producto = productos.codigo_producto
 GROUP BY usuarios.numero_documento;
 
--- (9) ¿Cuáles son los productos más vendidos dentro de cada categoría?
-SELECT categorias.nombre AS categoria, productos.nombre AS producto, SUM(pedidos_productos.cantidad) AS total_vendido
-FROM categorias
-JOIN productos ON categorias.codigo_categoria = productos.codigo_categoria
-JOIN pedidos_productos ON productos.codigo_producto = pedidos_productos.codigo_producto
-GROUP BY categorias.nombre, productos.nombre
-ORDER BY categorias.nombre, total_vendido DESC;
+-- (9) ¿Cuáles son los productos más vendidos dentro de cada categoría? Devolver solamente los nombres de los productos y su respectiva categoría.
+WITH ventas_categoria AS (
+    SELECT categorias.nombre AS categoria, productos.nombre AS producto, 
+           SUM(pedidos_productos.cantidad) AS total_vendido,
+           ROW_NUMBER() OVER (PARTITION BY categorias.nombre ORDER BY SUM(pedidos_productos.cantidad) DESC) AS ranking
+    FROM categorias
+    JOIN productos ON categorias.codigo_categoria = productos.codigo_categoria
+    JOIN pedidos_productos ON productos.codigo_producto = pedidos_productos.codigo_producto
+    GROUP BY categorias.nombre, productos.nombre
+)
+SELECT categoria, producto
+FROM ventas_categoria
+WHERE ranking = 1;
 
 -- (10) ¿Qué porcentaje de productos fueron entregados en la Ciudad Autónoma de Buenos Aires (CABA)?
 SELECT (COUNT(*) * 100.0 / (SELECT COUNT(*) FROM entregas)) AS porcentaje_entregas_caba
